6d830dc9cb82626dadc1fef4bea592c333f60982
Jasper van Rijbroek <jasper199069@live.nl>
2014-03-20 13:19:38 +0100

    Updated the install and uninstall sql files.

--- packages/component/administrator/components/com_carousel/install/uninstall.sql
+++ packages/component/administrator/components/com_carousel/install/uninstall.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS `#__carousel_items`;
 DROP TABLE IF EXISTS `#__carousel_categories`;
+
+DELETE * FROM `#__cck_fieldsets` WHERE `cck_fieldset_id` = 1200;
+DELETE * FROM `#__cck_fieldsets_elements` WHERE `cck_fieldset_id` = 1200;
